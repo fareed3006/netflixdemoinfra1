8a5b61492b8ad8663431f44d0f6679e492f0bdf0
Fareed Gani <137268191+fareed3006@users.noreply.github.com>
2025-10-28 11:40:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
   count                  = 4
   ami                    = "ami-02d26659fd82cf299"
   instance_type          = "t3.micro"
-  key_name               = "Netflix"
+  key_name               = "jenkins"
   vpc_security_group_ids = ["sg-01287f45771fbd601"]
   tags = {
     Name = var.instance_names[count.index]
